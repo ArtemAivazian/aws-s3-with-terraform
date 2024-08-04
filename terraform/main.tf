c5ceb7774d93e5b645ef7ee922292a18f84b914b
Aivazian, Artem <aivazart@fel.cvut.cz>
2024-08-04 15:00:54 +0200

    feat: make S3 bucket public
    ASWT-2 Make S3 bucket public

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,30 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 }
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_access" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.bucket_ownership,
+    aws_s3_bucket_public_access_block.bucket_access,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
